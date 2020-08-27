1433edf27f2c1ea47f7f57484fa953426b4612ba
Lin Qiu <theonlylinqiu@gmail.com>
2020-08-27 09:42:49 -0400

    Add SEO optimization

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,22 @@ export default function Home() {
     <div>
       <Head>
         <title>Lin Qiu's Personal Site</title>
-        <meta property="og:title" content="Lin Qiu's Personal Site" key="title" />
-        <meta property="description" content={`Lin Qiu is a software engineer with over ${expInYears} years of experience in the DC startup scene.`} />
+        <meta property="og:title" content={`Software Engineer with over ${expInYears} years of Experience in startups`} key="title" />
+        <meta name="description" property="description" content={`Lin Qiu is a software engineer with over ${expInYears} years of experience in the DC startup scene.`} />
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="Engineer, Ruby, Typescript, HTML, CSS, JavaScript" />
+        <meta name="author" content="Lin Qiu" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Grid container spacing={2} direction="column" justify="center" alignItems="center">
         <Grid item xs={12}>
-          <a href="https://linqiu.com"><img src="/me.png" /></a>
+          <a href="https://www.linqiu.com"><img src="/me.png" alt="this is my face"/></a>
         </Grid>
         <Grid item xs={12} container direction="column" justify="center" alignItems="center">
-          I am a software engineer with over {expInYears} years of experience in the startup scene. I taught David Hasselhoff Chinese once.
+          <h1>About me</h1>
+          <Grid item xs={12} sm={6}>
+            <div className={styles.description}>I am a self motivated full stack engineer with over {expInYears} years of experience in the DC startup scene. I am a serial startup entrepreneur and practice the philosophy of lean startup. I’m constantly involved in the tech community. I go to startup weekend at least once a year and build a demo-able product at the end (and it usually wins something). Don’t let my film background fool you, I put the tenets of narrative montage and user experience into every app I develop. Ask me about how I taught David Hasselhoff Chinese :)</div>
+          </Grid>
         </Grid>
 
         <Grid item xs={12} container direction="row" justify="center" alignItems="center">
